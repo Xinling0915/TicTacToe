59093dbb3cf3f85b2699f05f12e6fc9d616edb9f
xinling <xinling0915@gmail.com>
2022-06-17 22:56:21 +0800

    dice v3

--- app/src/main/java/com/example/noughtscrosses/MainActivity.kt
+++ app/src/main/java/com/example/noughtscrosses/MainActivity.kt
@@ -1,34 +1,21 @@
 package com.example.noughtscrosses
 
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
-import android.hardware.SensorManager
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.view.GestureDetector
-import android.view.MotionEvent
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.example.noughtscrosses.databinding.ActivityMainBinding
 import com.google.firebase.firestore.Query
-import kotlinx.coroutines.delay
-import java.util.*
 import kotlin.collections.HashMap
-import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
     var db = FirebaseFirestore.getInstance()
     lateinit var binding: ActivityMainBinding
-    var user: MutableMap<String, Any> = HashMap()
     var winCount = 1
     var Players: MutableMap<String, Any> = HashMap()
     lateinit var mper: MediaPlayer
-    lateinit var gDetector: GestureDetector
-    var sensorManager: SensorManager? = null
-    var sensor: Sensor? = null
 
     //1=green  0 =red
     var activePlayer = 1
@@ -187,21 +174,20 @@ class MainActivity : AppCompatActivity() {
     fun home(view: View?) {
         binding.gridLayout.visibility = View.GONE
         binding.winner.visibility = View.GONE
-        binding.player1Data.visibility = View.GONE
-        binding.player2Data.visibility = View.GONE
+        binding.player1Image.visibility = View.GONE
+        binding.player2Image.visibility = View.GONE
+        binding.player1Name.visibility = View.GONE
+        binding.player2Name.visibility = View.GONE
         binding.player.visibility = View.VISIBLE
         binding.charts.visibility = View.VISIBLE
         binding.player1.setText("")
         binding.player2.setText("")
+        count = 0
     }
 
-    var players: String = ""
-    lateinit var player1: EditText
-    lateinit var player2: EditText
-    var player1name = ""
-    var player2name = ""
     var player1Count = 0
     var player2Count = 0
+    var rolldice = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -235,11 +221,10 @@ class MainActivity : AppCompatActivity() {
                 binding.roll.visibility = View.VISIBLE
                 binding.dice.setImageResource(R.drawable.dice)
                 binding.player2Name.setText(binding.player2.text.toString())
-                binding.player1Name.text = "請玩家" + binding.player1.text.toString() + "骰骰子"
-
+                binding.player1Name.setText("請玩家" + binding.player1.text.toString() + "骰骰子")
                 activePlayer = 1
                 gameIsActive = true
-                count = 0
+
                 for (i in gameState.indices) {
                     gameState[i] = 2
                 }
@@ -254,26 +239,21 @@ class MainActivity : AppCompatActivity() {
 
         binding.roll.setOnClickListener({
             rndDice()
-            player1Count = counter
-            if(player1Count>0) {
-                binding.player1Name.text = "玩家" + binding.player1.text.toString() + "骰子數" + player1Count
-                binding.player2Name.text = "請玩家" + binding.player2.text.toString() + "骰骰子"
-                binding.roll.setOnClickListener({
-                    rndDice()
-                    player2Count = counter
-                    binding.player2Name.text = "玩家" + binding.player2.text.toString() + "骰子數" + player2Count
-                    if(player2Count>0) {
-                        if (player1Count > player2Count) {
-                            binding.startGame.visibility = View.VISIBLE
-                            binding.start.text = "由玩家" + binding.player1.text.toString() + "先開始遊戲"
-                        } else if (player1Count < player2Count) {
-                            binding.startGame.visibility = View.VISIBLE
-                            binding.start.text = "由玩家" + binding.player2.text.toString() + "先開始遊戲"
-                            player1name += binding.player2Name.text
-                            player2name += binding.player1Name.text
-                        }
-                    }
-                })
+            if (rolldice == 1 && counter>0) {
+                player1Count = counter
+                binding.player1Name.setText("玩家" + binding.player1.text.toString() + "骰子數" + player1Count)
+                binding.player2Name.setText("請玩家" + binding.player2.text.toString() + "骰骰子")
+            } else if (rolldice == 2 && counter>0) {
+                rolldice = 0
+                player2Count = counter
+                binding.player2Name.setText("玩家" + binding.player2.text.toString() + "骰子數" + player2Count)
+                if (player1Count > player2Count) {
+                    binding.startGame.visibility = View.VISIBLE
+                    binding.start.setText("由玩家" + binding.player1.text.toString() + "先開始遊戲")
+                } else if (player1Count < player2Count) {
+                    binding.startGame.visibility = View.VISIBLE
+                    binding.start.setText("由玩家" + binding.player2.text.toString() + "先開始遊戲")
+                }
             }
         })
 
@@ -283,18 +263,19 @@ class MainActivity : AppCompatActivity() {
             binding.gridLayout.visibility = View.VISIBLE
             binding.player1Image.visibility = View.VISIBLE
             binding.player2Image.visibility = View.VISIBLE
-            if(player2Count>0) {
-                binding.player1Name.text = binding.player1.text.toString()
-                binding.player2Name.text = binding.player2.text.toString()
-            }else{
-                binding.player1Name.setText(player1name)
-                binding.player2Name.setText(player2name)
+            if (player1Count > player2Count) {
+                binding.player1Name.setText(binding.player1.text.toString())
+                binding.player2Name.setText(binding.player2.text.toString())
+            } else if (player1Count < player2Count) {
+                binding.player1Name.setText(binding.player2.text.toString())
+                binding.player2Name.setText(binding.player1.text.toString())
             }
         })
     }
 
     var counter = 0
-    fun rndDice():Int{
+    fun rndDice():Int {
+        rolldice++
         counter = (1..6).random()
             when (counter) {
                 1 -> binding.dice.setImageResource(R.drawable.dice1)
